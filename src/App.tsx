496b72a2efca37be1aaf9e0b725009008859f549
CHR-onicles <tpandivine48@gmail.com>
2022-11-13 23:33:21 +0000

    Added global styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,50 @@
 import { Container } from "@styles/components/Container.styled";
+import { GlobalStyles } from "@styles/GlobalStyles.styled";
 import { StyledApp } from "./App.styled";
 import reactLogo from "./assets/react.svg";
 
 export const App = () => {
   return (
-    <StyledApp>
-      <Container>
-        <nav>
-          <h2>SC-template app</h2>
-          <img src={reactLogo} width={35} height={35} />
-        </nav>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae et,
-          sed, ipsam voluptates iste atque nesciunt nemo deserunt deleniti
-          provident, eligendi odio corrupti accusamus? Quam, optio. Tenetur
-          ullam, suscipit earum error accusamus, maxime assumenda beatae debitis
-          deleniti molestiae quae necessitatibus! provident, eligendi odio
-          corrupti accusamus? Quam, optio. Tenetur ullam, suscipit earum error
-          accusamus, maxime assumenda beatae debitis deleniti molestiae quae
-          necessitatibus! provident, eligendi odio corrupti accusamus? Quam,
-          optio. Tenetur ullam, suscipit earum error accusamus, maxime assumenda
-          beatae debitis deleniti molestiae quae necessitatibus! provident,
-          eligendi odio corrupti accusamus? Quam, optio. Tenetur ullam, suscipit
-          earum error accusamus, maxime assumenda beatae debitis deleniti
-          molestiae quae necessitatibus!
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae et,
-          sed, ipsam voluptates iste atque nesciunt nemo deserunt deleniti
-          provident, eligendi odio corrupti accusamus? Quam, optio. Tenetur
-          ullam, suscipit earum error accusamus, maxime assumenda beatae debitis
-          deleniti molestiae quae necessitatibus! provident, eligendi odio
-          corrupti accusamus? Quam, optio. Tenetur ullam, suscipit earum error
-          accusamus, maxime assumenda beatae debitis deleniti molestiae quae
-          necessitatibus! provident, eligendi odio corrupti accusamus? Quam,
-          optio. Tenetur ullam, suscipit earum error accusamus, maxime assumenda
-          beatae debitis deleniti molestiae quae necessitatibus! provident,
-          eligendi odio corrupti accusamus? Quam, optio. Tenetur ullam, suscipit
-          earum error accusamus, maxime assumenda beatae debitis deleniti
-          molestiae quae necessitatibus!
-        </p>
-      </Container>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Container>
+          <nav>
+            <h2>SC-template app</h2>
+            <img src={reactLogo} width={35} height={35} />
+          </nav>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae et,
+            sed, ipsam voluptates iste atque nesciunt nemo deserunt deleniti
+            provident, eligendi odio corrupti accusamus? Quam, optio. Tenetur
+            ullam, suscipit earum error accusamus, maxime assumenda beatae
+            debitis deleniti molestiae quae necessitatibus! provident, eligendi
+            odio corrupti accusamus? Quam, optio. Tenetur ullam, suscipit earum
+            error accusamus, maxime assumenda beatae debitis deleniti molestiae
+            quae necessitatibus! provident, eligendi odio corrupti accusamus?
+            Quam, optio. Tenetur ullam, suscipit earum error accusamus, maxime
+            assumenda beatae debitis deleniti molestiae quae necessitatibus!
+            provident, eligendi odio corrupti accusamus? Quam, optio. Tenetur
+            ullam, suscipit earum error accusamus, maxime assumenda beatae
+            debitis deleniti molestiae quae necessitatibus!
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae et,
+            sed, ipsam voluptates iste atque nesciunt nemo deserunt deleniti
+            provident, eligendi odio corrupti accusamus? Quam, optio. Tenetur
+            ullam, suscipit earum error accusamus, maxime assumenda beatae
+            debitis deleniti molestiae quae necessitatibus! provident, eligendi
+            odio corrupti accusamus? Quam, optio. Tenetur ullam, suscipit earum
+            error accusamus, maxime assumenda beatae debitis deleniti molestiae
+            quae necessitatibus! provident, eligendi odio corrupti accusamus?
+            Quam, optio. Tenetur ullam, suscipit earum error accusamus, maxime
+            assumenda beatae debitis deleniti molestiae quae necessitatibus!
+            provident, eligendi odio corrupti accusamus? Quam, optio. Tenetur
+            ullam, suscipit earum error accusamus, maxime assumenda beatae
+            debitis deleniti molestiae quae necessitatibus!
+          </p>
+        </Container>
+      </StyledApp>
+    </>
   );
 };
