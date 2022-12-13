977a53c7dd304bd2e93a05e5335ba4efc10c636c
CHR-onicles <tpandivine48@gmail.com>
2022-12-13 00:46:49 +0000

    chore: Upgraded packages
    - Removed tsconfig.node.json as I don't see the use

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Container } from "@styles/components/Container.styled";
 import { GlobalStyles } from "@styles/GlobalStyles.styled";
 
-import reactLogo from "./assets/react.svg";
+import { ReactComponent as Logo } from "./assets/react.svg";
 import { StyledApp } from "./App.styled";
-
 
 export const App = () => {
   return (
@@ -13,7 +12,7 @@ export const App = () => {
         <Container>
           <nav>
             <h2>SC-template app</h2>
-            <img src={reactLogo} width={35} height={35} alt="" />
+            <Logo />
           </nav>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae et,
