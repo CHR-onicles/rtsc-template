62ad327f3bbb6b6da7b563906ce31314c7a3f9b5
Divine Anum <tpandivine48@gmail.com>
2023-10-08 23:57:51 +0000

    chore: Updated utilities and tests, resolves #264

--- src/styles/Utilities.styled.ts
+++ src/styles/Utilities.styled.ts
@@ -10,54 +10,8 @@ export const rem = (value: number, omitUnit = false) => {
   return value === 0 ? "0" : `${value / 16}${omitUnit === true ? "" : "rem"}`;
 };
 
-/**
- * Helper function to add alpha values to hex and hsl colors.
- * @param color string representing color in hex or hsl.
- * @param opacity number representing level of opacity.
- * @returns string containing color with modified opacity.
- */
-export const alpha = (color: string, opacity = 1) => {
-  const hex_pattern = /^#([A-Fa-f0-9]{6})$/;
-  const hsl_pattern =
-    /hsl\(\s*([0-2]?[0-9]{1,2}|3[0-5][0-9]|360)\s*,\s*(0|[1-9][0-9]?|100)%\s*,\s*(0|[1-9][0-9]?|100)%\s*\)/; // chose to be explicit on the digits to prevent evaluating wrong digits
-  const hsl_pattern_with_opacity =
-    /hsla?\(\s*([0-2]?[0-9]{1,2}|3[0-5][0-9]|360)\s*,\s*(0|[1-9][0-9]?|100)%\s*,\s*(0|[1-9][0-9]?|100)%\s*,\s*0(\.\d+)?|1\s*\)/;
-  let newColor = "";
-
-  if (opacity < 0 || opacity > 1) {
-    throw new Error("Opacity must be between 0 and 1 inclusive");
-  }
-  const formatted_opacity = +String(opacity).padStart(3, "0");
-
-  if (color.includes("#")) {
-    if (!hex_pattern.test(color))
-      throw new Error(
-        "Invalid hexadecimal - Value provided must follow the 6-character syntax (#FFFFFF)"
-      );
-    else
-      return `${color}${Math.floor(formatted_opacity * 255)
-        .toString(16)
-        .padStart(2, "0")}`;
-  }
-
-  if (hsl_pattern.test(color)) {
-    newColor = color.slice(0, color.length - 1) + `,${formatted_opacity})`;
-  } else if (hsl_pattern_with_opacity.test(color)) {
-    const temp = color.split(",");
-    newColor = temp.reduce((acc, item, index) => {
-      if (index === temp.length - 1) return `${acc},${String(opacity)})`;
-      if (index > 0) return acc + "," + item;
-      else return acc + item;
-    }, "");
-  }
-
-  // console.log(newColor, color);
-  if (!newColor)
-    throw new Error(
-      `Invalid color provided(${newColor}) - only hex and hsl are supported`
-    ); // ignore rgb - might as well make it hsl :)
-  return newColor;
-};
+export const alpha = (color: string, opacity = 1) =>
+  `color-mix(in srgb, ${color} ${opacity * 100}%, rgb(0,0,0,0))`;
 
 // Breakpoints (mobile up)
 export const bp = {
@@ -80,36 +34,6 @@ export const mq = {
   large4: `(min-width: ${bp.large4})`,
   extreme: `(min-width: ${bp.extreme})`,
 };
-
-// export const theme = {
-//   colors: {
-//     primary400: "",
-
-//     // NEUTRAL
-//     white: "hsl(0, 0%, 100%)",
-//     black: "hsl(0, 0%, 0%)",
-//     gray100: "	hsl(220, 23%, 97%)",
-//     gray200: "hsl(0, 0%, 77%)",
-//     gray300: "hsl(0, 0%, 50%)",
-//     gray700: "hsl(0, 0%, 12%)",
-//   },
-//   fonts: {
-//     primary: `'Poppins', sans-serif`,
-//     accent: `serif`,
-//   },
-//   ease: {
-//     // easing functions
-//     slowStart: "cubic-bezier(0.52, 0.01, 0, 1)",
-//     quickStartAndEnd: "cubic-bezier(0.19, 1, 0.22, 1)",
-//     almostLinear: "cubic-bezier(0.4,0.6,0.6,1)",
-//     textReveal: "cubic-bezier(0.77, 0, 0.175, 1)",
-//   },
-//   borderRadius: {
-//     small: rem(5),
-//     medium: rem(10),
-//     large: rem(15),
-//   },
-// };
 
 /*********************************************************************
 CSS UTILITIES (similar to Sass mixins)
@@ -158,9 +82,19 @@ export const size = (width: string, height = width) => css`
   height: ${height};
 `;
 
-export const circle = (circleSize: string) => css`
-  ${size(circleSize)}
+export const circle = (diameter: string) => css`
+  ${size(diameter)}
   border-radius: 50%;
+`;
+
+export const marginInlineAuto = (width: string) => css`
+  margin-inline: auto;
+  width: ${width};
+`;
+
+export const clipBorderRadius = (borderRadius: string) => css`
+  border-radius: ${borderRadius};
+  overflow: hidden;
 `;
 
 export const media = (bp: string) => `@media screen and (min-width: ${bp})`;
--- src/styles/Utilities.test.ts
+++ src/styles/Utilities.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 
-import { alpha, bp, mq, rem } from "./Utilities.styled";
+import { bp, mq, rem } from "./Utilities.styled";
 
 
 describe("Properly convert from px to rem", () => {
@@ -17,44 +17,6 @@ describe("Properly convert from px to rem", () => {
 
   test("16px with omitUnit flag as true appended with another unit returns 1 + appended unit", () =>
     expect(rem(16, true) + "vh").toBe("1vh"));
-});
-
-describe("Properly add alpha values to hex and hsl colors", () => {
-  test("Throw error if hex value is wrong", () => {
-    expect(() => alpha("#g125ff")).toThrow(Error); // wrap expected error function to prevent it from executing and failing the test
-  });
-
-  test("Throw error if hex value is less than 6 characters", () => {
-    expect(() => alpha("#fff")).toThrow(Error);
-  });
-
-  test("Throw error if hex value is more than 6 characters", () => {
-    expect(() => alpha("#fffffff")).toThrow(Error);
-  });
-
-  test("Properly add opacity to hex values", () => {
-    expect(alpha("#ffffff", 0)).toBe("#ffffff00");
-  });
-
-  test("Throw error if opacity argument for alpha function is greater than 1", () => {
-    expect(() => alpha("hsl(0,0%,0%)", 2)).toThrow(Error);
-  });
-
-  test("Throw error if opacity argument for alpha function is less than 0", () => {
-    expect(() => alpha("hsl(0,0%,0%)", -1)).toThrow(Error);
-  });
-
-  test("Throw error if rgb or any other wrong format is provided", () => {
-    expect(() => alpha("rgb(0,0%,0%)", 0.3)).toThrow(Error);
-  });
-
-  test("Alpha function should accept and convert hsl values", () => {
-    expect(alpha("hsl(0,0%,0%)", 0.2)).toBe("hsl(0,0%,0%,0.2)");
-  });
-
-  test("Alpha function should accept and convert hsl values with opacity already provided", () => {
-    expect(alpha("hsl(0, 0%, 0%, 0.5)", 0.1)).toBe("hsl(0, 0%, 0%,0.1)"); // "hsl" and "hsla" are aliases and both support alpha values so I'll stick with "hsl"
-  });
 });
 
 describe("Media queries work as intended", () => {
