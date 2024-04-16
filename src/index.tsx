a0967cdf6e0e467bdc4160eadb1c68fc24caf4c4
Divine Anum <tpandivine48@gmail.com>
2024-04-16 23:03:24 +0000

    chore: Update dependencies and setup react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StyleSheetManager } from "styled-components";
 
 import { App } from "./App";
 
 
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error: any, query) => {
+      //  errorToast("Something went really wrong: " + error.message);
+      import.meta.env.DEV && console.error(error);
+
+      // 🎉 only show error toasts if we already have data in the cache
+      // which indicates a failed background update
+      if (query.state.data !== undefined) {
+        // toast.error(`Something went wrong: ${error.message}`);
+        import.meta.env.DEV && console.error("Background update:", error);
+      }
+    },
+  }),
+
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <StyleSheetManager enableVendorPrefixes>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <App />
+      </QueryClientProvider>
     </StyleSheetManager>
   </React.StrictMode>
 );
